464a7530af4f1d8e1de0a0def79f16418ecc76e8
Your Name <you@example.com>
2024-07-29 21:03:59 +0530

    Component added

---
+++ src/components/GoalList/GoalList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./GoalList.css"
+const GoalList = () => {
+
+
+    return (
+        <div>
+
+            <ul className="list">
+                <li>REACT</li>
+                <li>NODE</li>
+                <li>MONGO</li>
+                <li>ANGULAR</li>
+            </ul>
+        </div>);
+
+}
+
+export default GoalList;
